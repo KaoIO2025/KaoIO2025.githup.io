6daca1dea9c7b9b3f295101f5f01989d9789d597
KaoIO2025 <kao191600@gmail.com>
2025-05-01 00:04:54 +0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+// App.jsx
+import ExpenseTracker from './ExpenseTracker';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <ExpenseTracker />
+    </div>
+  );
+}
